c20328b4eaf0b199a2e2d2e8c1d2950333adb9b1
youmu129 <youmu129@126.com>
2022-03-29 16:44:52 +0800

    Updated winmd

--- cppwin32/winmd/impl/winmd_reader/cache.h
+++ cppwin32/winmd/impl/winmd_reader/cache.h
@@ -7,8 +7,8 @@ namespace winmd::reader
         cache(cache const&) = delete;
         cache& operator=(cache const&) = delete;
 
-        template<typename C, typename T = typename C::value_type>
-        explicit cache(C const& files)
+        template<typename C, typename T = typename C::value_type, typename TypeFilter>
+        explicit cache(C const& files, TypeFilter filter)
         {
             for (auto&& file : files)
             {
@@ -16,7 +16,7 @@ namespace winmd::reader
 
                 for (auto&& type : db.TypeDef)
                 {
-                    if (type.Flags().value == 0 || is_nested(type))
+                    if (type.Flags().value == 0 || is_nested(type) || !filter(type))
                     {
                         continue;
                     }
@@ -35,41 +35,32 @@ namespace winmd::reader
             {
                 for (auto&&[name, type] : members.types)
                 {
-                    switch (get_category(type))
-                    {
-                    case category::interface_type:
-                        members.interfaces.push_back(type);
-                        continue;
-                    case category::class_type:
-                        if (extends_type(type, "System"sv, "Attribute"sv))
-                        {
-                            members.attributes.push_back(type);
-                            continue;
-                        }
-                        members.classes.push_back(type);
-                        continue;
-                    case category::enum_type:
-                        members.enums.push_back(type);
-                        continue;
-                    case category::struct_type:
-                        if (get_attribute(type, "Windows.Foundation.Metadata"sv, "ApiContractAttribute"sv))
-                        {
-                            members.contracts.push_back(type);
-                            continue;
-                        }
-                        members.structs.push_back(type);
-                        continue;
-                    case category::delegate_type:
-                        members.delegates.push_back(type);
-                        continue;
-                    }
+                    add_type_to_members(type, members);
                 }
             }
         }
 
-        explicit cache(std::string const& file) : cache{ std::vector<std::string>{ file } }
+        template<typename C, typename T = typename C::value_type>
+        explicit cache(C const& files) : cache{ files, default_type_filter{} }
         {
         }
+
+        template <typename TypeFilter>
+        explicit cache(std::string const& file, TypeFilter filter) : cache{ std::vector<std::string>{ file }, filter }
+        {
+        }
+
+        explicit cache(std::string const& file) : cache{ std::vector<std::string>{ file }, default_type_filter{} }
+        {
+        }
+
+        struct default_type_filter
+        {
+            bool operator()(TypeDef const&) const noexcept
+            {
+                return true;
+            }
+        };
 
         TypeDef find(std::string_view const& type_namespace, std::string_view const& type_name) const noexcept
         {
@@ -165,6 +156,38 @@ namespace winmd::reader
             remove(members.delegates, name);
         }
 
+        // This won't invalidate any existing database or row_base (e.g. TypeDef) instances
+        // However, it may invalidate iterators and references to namespace_members, because those are stored in std::vector
+        template <typename TypeFilter>
+        void add_database(std::string_view const& file, TypeFilter filter)
+        {
+            auto& db = m_databases.emplace_back(file, this);
+            for (auto&& type : db.TypeDef)
+            {
+                if (type.Flags().value == 0 || is_nested(type) || !filter(type))
+                {
+                    continue;
+                }
+
+                auto& ns = m_namespaces[type.TypeNamespace()];
+                auto[iter, inserted] = ns.types.try_emplace(type.TypeName(), type);
+                if (inserted)
+                {
+                    add_type_to_members(type, ns);
+                }
+            }
+
+            for (auto&& row : db.NestedClass)
+            {
+                m_nested_types[row.EnclosingType()].push_back(row.NestedType());
+            }
+        }
+
+        void add_database(std::string_view const& file)
+        {
+            add_database(file, default_type_filter{});
+        }
+
         std::vector<TypeDef> const& nested_types(TypeDef const& enclosing_type) const
         {
             auto it = m_nested_types.find(enclosing_type);
@@ -194,6 +217,38 @@ namespace winmd::reader
         using namespace_type = std::pair<std::string_view const, namespace_members> const&;
 
     private:
+
+        void add_type_to_members(TypeDef const& type, namespace_members& members)
+        {
+            switch (get_category(type))
+            {
+            case category::interface_type:
+                members.interfaces.push_back(type);
+                return;
+            case category::class_type:
+                if (extends_type(type, "System"sv, "Attribute"sv))
+                {
+                    members.attributes.push_back(type);
+                    return;
+                }
+                members.classes.push_back(type);
+                return;
+            case category::enum_type:
+                members.enums.push_back(type);
+                return;
+            case category::struct_type:
+                if (get_attribute(type, "Windows.Foundation.Metadata"sv, "ApiContractAttribute"sv))
+                {
+                    members.contracts.push_back(type);
+                    return;
+                }
+                members.structs.push_back(type);
+                return;
+            case category::delegate_type:
+                members.delegates.push_back(type);
+                return;
+            }
+        }
 
         std::list<database> m_databases;
         std::map<std::string_view, namespace_members> m_namespaces;
